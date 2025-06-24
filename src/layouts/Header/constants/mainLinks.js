dca2af429ea722c77fd6e0fbcbbf91e1ffc6dd1a
Oleh Bilenkyi <i3eLuy@gmail.com>
2025-06-25 00:44:01 +0200

    { to: "/pricing", label: "Nasze Ceny" }, // Соответствует Pricing

--- src/layouts/Header/constants/mainLinks.js
+++ src/layouts/Header/constants/mainLinks.js
@@ -1,8 +1,8 @@
 export const MAIN_LINKS = [
   { to: "/", label: "Strona glowna" }, // Соответствует HomePage
   { to: "/menu-selection", label: "Menu do wyboru" }, // Соответствует MenuSelectionPage
-  { to: "/pricing", label: "Nasze Ceny" }, // Соответствует Pricing
   { to: "/order", label: "Standardowe menu" }, // Соответствует OrderPage
+  { to: "/pricing", label: "Nasze Ceny" }, // Соответствует Pricing
   { to: "/privacy-policy", label: "Polityka" }, // Соответствует PrivacyPolicy
   { to: "/regulamin", label: "Regulamin" }, // Соответствует RegulaminPage
 ];
